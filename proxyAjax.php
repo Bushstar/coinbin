b0e4579059f35306ec40b3e2a309b7c41bf2f03d
Peter Bushnell <bushsolo@gmail.com>
2018-01-16 12:53:49 +0000

    Update proxy

--- proxyAjax.php
+++ proxyAjax.php
@@ -3,5 +3,8 @@
 // ... validation of params
 // and checking of url against whitelist would happen here ...
 // assume that $url now contains "http://stackoverflow.com/10m"
-$url = htmlspecialchars($_GET["url"]);
-echo file_get_contents($url);
+$url = "";
+if (isset($_GET["url"]))
+    $url = htmlspecialchars($_GET["url"]);
+if ($url != "")
+    echo file_get_contents($url);
